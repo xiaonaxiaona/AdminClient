9c7749152fd5d8e6f456c70692f8dcb4b8447f51
lina <123456@sina.com>
2019-06-14 17:08:12 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React,{Component} from 'react';
+import {BrowserRouter,Route,Switch,} from 'react-router-dom'
+import { Button } from 'antd';
+
+import Admin from './pages/admin/admin'
+import Login from './pages/login/login'
+
+export default class App extends Component {
+ render() {
+  return (
+   <BrowserRouter>
+    <Switch>
+     <Route path='/admin' component={Admin}></Route>
+     <Route path='/login' component={Login}></Route>
+    </Switch>
+   </BrowserRouter>   
+  )
+ }
+};
+
+
+
---
+++ src/pages/login/login.jsx
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+import {Button} from 'antd'
+
+export default class Login extends Component {
+ render() {
+  return (
+   <div className='login'>
+    <header></header>
+   </div>
+  )
+ }
+};
